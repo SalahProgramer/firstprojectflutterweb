dc249ac467db0c28d70a9b6a6dd5f8e96b9256ea
Salah Mustafa Yaaqba <132152831+SalahProgramer@users.noreply.github.com>
2025-11-16 12:29:07 +0200

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -15,7 +15,7 @@ echo "=== Getting dependencies ==="
 flutter pub get
 
 echo "=== Building Flutter web (release) ==="
-flutter build web --release --web-renderer canvaskit
+flutter build web --release
 
 echo "=== Build complete. Output at build/web ==="
 
